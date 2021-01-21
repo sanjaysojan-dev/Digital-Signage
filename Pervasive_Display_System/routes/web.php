87415b4d1afe155fa77ad443590153360dfd1a31
sanjay <tecdevse@gmail.com>
2021-01-21 11:31:13 +0000

    Added routing options to login and register page to aid navigation

--- Pervasive_Display_System/routes/web.php
+++ Pervasive_Display_System/routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('wiki');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
