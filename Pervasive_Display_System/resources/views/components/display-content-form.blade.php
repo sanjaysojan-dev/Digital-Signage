0fd9fffcf18e5185e79cccdd8060916578ff057c
sanjay <tecdevse@gmail.com>
2021-01-27 19:14:09 +0000

    Created NodeContentController

--- Pervasive_Display_System/resources/views/components/display-content-form.blade.php
+++ Pervasive_Display_System/resources/views/components/display-content-form.blade.php
@@ -6,14 +6,14 @@
         @csrf
 
         <div class="mt-2">
-            <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" type="text" name="title"
+            <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" type="text" name="contentTitle"
                    placeholder="Title" required>
         </div>
 
         <span class="text-l text-blue-600 pb-4">~ Enter Image Title</span>
 
         <div class="mt-2">
-            <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="description"
+            <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="contentDescription"
                       placeholder='Type Your Review' required></textarea>
         </div>
 
