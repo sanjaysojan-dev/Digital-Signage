765ef5cd347cb7cb7b67ebad8641b94a11d735ef
sanjay <tecdevse@gmail.com>
2021-04-15 02:14:22 +0100

    Minor modifications to scripts - need to clear config before testing

--- Run_Broswer_Test.bat
+++ Run_Broswer_Test.bat
@@ -1,2 +1,3 @@
+php artisan config:clear
 php artisan migrate:refresh
 php artisan dusk --log-junit browser_tests.xml
--- Run_Feature_Test.bat
+++ Run_Feature_Test.bat
@@ -1,2 +1,3 @@
+php artisan config:clear
 php artisan migrate:refresh
 php artisan test --log-junit feature_tests.xml
